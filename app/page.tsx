6163a8095825871ace91f1869ec53a21fabc5e67
Collin Morrison <morricollin@gmail.com>
2025-03-02 21:59:40 -0700

    finally got the images to line up correctly

--- app/page.tsx
+++ app/page.tsx
@@ -2,134 +2,51 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    //   <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-    //     <Image
-    //       className="dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js logo"
-    //       width={180}
-    //       height={38}
-    //       priority
-    //     />
-    //     <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-    //       <li className="mb-2">
-    //         Get started by editing{" "}
-    //         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-    //           app/page.tsx
-    //         </code>
-    //         .
-    //       </li>
-    //       <li>Save and see your changes instantly.</li>
-    //     </ol>
-
-    //     <div className="flex gap-4 items-center flex-col sm:flex-row">
-    //       <a
-    //         className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <Image
-    //           className="dark:invert"
-    //           src="/vercel.svg"
-    //           alt="Vercel logomark"
-    //           width={20}
-    //           height={20}
-    //         />
-    //         Deploy now
-    //       </a>
-    //       <a
-    //         className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Read our docs
-    //       </a>
-    //     </div>
-    //   </main>
-    //   <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-    //     <a
-    //       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <Image
-    //         aria-hidden
-    //         src="/file.svg"
-    //         alt="File icon"
-    //         width={16}
-    //         height={16}
-    //       />
-    //       Learn
-    //     </a>
-    //     <a
-    //       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <Image
-    //         aria-hidden
-    //         src="/window.svg"
-    //         alt="Window icon"
-    //         width={16}
-    //         height={16}
-    //       />
-    //       Examples
-    //     </a>
-    //     <a
-    //       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    //       href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <Image
-    //         aria-hidden
-    //         src="/globe.svg"
-    //         alt="Globe icon"
-    //         width={16}
-    //         height={16}
-    //       />
-    //       Go to nextjs.org →
-    //     </a>
-    //   </footer>
-    // </div>
     <>
     <header id="header"></header>
-    <main>
-      <div className="w-full h-16 shadow-lg">
-                <div className="h-full mr-10 ml-10 flex justify-end items-center">
-                    <a href="/login" className="text-white mr-10">Login</a>
-                    <a href="/register" className="text-white">Register</a>
-                </div>
-            </div>
-      <div className="h-7">
-
-      </div>
-        <div className="bg-gray-400 flex justify-center">
-          <div className="flex flex-col">
-            <h2 className="text-5xl font-extrabold row p-8">Welcome to Realms</h2>
-            <p className="row">Explore the world of Realms, create your characters, and define their powers.</p>
-            <a href="/login" >Get Started</a>
+    <main className="">
+      <div className="w-full h-16 text-053357">
+        <div className="h-full flex flex-row justify-center items-center">
+          <div className="h-full mr-10 ml-10 flex justify-self-end justify-end items-center">
+              <Image src="/logo-mini.png" alt="logo-mini" width={50} height={50} />
+          </div>
+          <div className="h-full w-full flex justify-center items-center pl-17">
+            <a href="/login" className="mr-10">Characters</a>
+            <a href="/register" className="mr-10">Library</a>
+            <a href="/login" className="mr-10">Rules</a>
+            <a href="/register" className="mr-10">Codex</a>
+            <a href="/login" className="mr-10">Creators</a>
+            <a href="/register" className="mr-10">RM Tools</a>
+            <a href="/register" className="mr-10">Campaigns</a>
+          </div>
+          <div className="h-full mr-10 ml-10 flex justify-self-end justify-end items-center">
+              <a href="/login">Login</a>
+              {/* <a href="/register" className="">Register</a> */}
           </div>
         </div>
-        <section >
-            <div >
-                <h3>Create Characters</h3>
-                <p>Design your unique characters with detailed attributes and backgrounds.</p>
-            </div>
-            <div >
-                <h3>Define Powers</h3>
-                <p>Customize and create powerful abilities for your characters.</p>
-            </div>
-            <div >
-                <h3>Join Adventures</h3>
-                <p>Participate in epic adventures and quests with other players.</p>
-            </div>
-        </section>
+    </div>
+    <div className="w-full h-[50vh] flex bg-[url('/vista.png')] bg-cover bg-center justify-center">
+      <div className="w-full flex justify-center items-center">
+        {/* <h2 className="text-5xl font-extrabold row p-8">Welcome to Realms</h2>
+        <p className="row">Explore the world of Realms, create your characters, and define their powers.</p>
+        <a href="/login" >Get Started</a> */}
+        <Image className="w-1/2 h-fit" src="/logo-full.png" alt="logo-full" width={242} height={100}/>
+      </div>
+    </div>
+    <section >
+        <div >
+            <h3>Create Characters</h3>
+            <p>Design your unique characters with detailed attributes and backgrounds.</p>
+        </div>
+        <div >
+            <h3>Define Powers</h3>
+            <p>Customize and create powerful abilities for your characters.</p>
+        </div>
+        <div >
+            <h3>Join Adventures</h3>
+            <p>Participate in epic adventures and quests with other players.</p>
+        </div>
+    </section>
     </main>
     <footer id="footer"></footer>
     </>
